b48aba6e14dfd7a73885ef4b4108f3f0ea4d50da
ajil23 <azrilpraya@gmail.com>
2023-08-17 09:30:13 +0800

    akun

--- resources/views/admin/akun.blade.php
+++ resources/views/admin/akun.blade.php
@@ -11,19 +11,9 @@
         </div>
     </div>
     <div class="bg-white rounded-lg w-80 mx-auto mb-5 overflow-hidden  mt-6">
-        <div id="dataAnak"></div>
-    </div>
-
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <div class="flex flex-col">
-            <input type="text" id="find" class="border rounded-lg border-gray-400 hover:border-black focus:border-black p-[8px_10px]" name="email" placeholder="Cari..." required > 
-        </div>
+        <div id="akun"></div>
     </div>
   
-    <div class="text-gray-600  mx-6 mb-5">
-        <h1>Data Anak</h1>
-        <hr class="border-gray-500 ">
-    </div>
     {{-- Data Anak --}}
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <div class="bg-white rounded-3xl shadow-xl">
@@ -31,8 +21,8 @@
             <div class="bg-red-500 bg-cover w-12 h-12 mr-2 rounded-[500px]"
                 style="background-image: url({{ asset('kader/foto/helmi.jpg') }});"></div>
             <div>
-                <h2 class="font-bold text-xs">Helmi Napan</h2>
-                <h2 class="font-extralight text-xs">Biodata bayi yang bersangkutan</h2>
+                <h2 class="font-bold text-xs">Kader Mawar 5</h2>
+                <h2 class="font-extralight text-xs">@mawar 5</h2>
             </div>
         </div>
       </div>
@@ -40,16 +30,35 @@
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <div class="bg-white rounded-3xl shadow-xl">
         <div class="flex items-center px-5 py-3">
-            <div class="bg-red-500 bg-cover w-12 h-12 mr-2 rounded-[500px]"
-                style="background-image: url({{ asset('kader/foto/helmi.jpg') }});"></div>
+            <div class="bg-white bg-cover w-12 h-12 mr-2 rounded-[500px]"
+                style="background-image: url({{ asset('kader/foto/settings.png') }});"></div>
             <div>
-                <h2 class="font-bold text-xs">Napan Helmi</h2>
-                <h2 class="font-extralight text-xs">Biodata bayi yang bersangkutan</h2>
+                <h2 class="font-bold text-md">Pengaturan</h2>
             </div>
         </div>
       </div>
     </div>
-    <button class="rounded-full w-14 h-14 flex fixed bottom-16 right-5 bg-green-600 text-[35px]">
-        <div class="mx-auto text-white"> <i class="fa-solid fa-plus" ></i></div>
-    </button>
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <div class="bg-white rounded-3xl shadow-xl">
+        <div class="flex items-center px-5 py-3">
+            <div class="bg-white bg-cover w-12 h-12 mr-2 rounded-[500px]"
+                style="background-image: url({{ asset('kader/foto/info.png') }});"></div>
+            <div>
+                <h2 class="font-bold text-md">Tentang</h2>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <div class="bg-red-500 rounded-3xl shadow-xl">
+        <div class="flex items-center px-5 py-3">
+            <div class="bg-red-500 bg-cover w-12 h-12 mr-2 rounded-[500px]"
+                style="background-image: url({{ asset('kader/foto/logout.png') }});"></div>
+            <div>
+                <h1 class="font-bold text-md text-white">Keluar</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+
 @endsection
