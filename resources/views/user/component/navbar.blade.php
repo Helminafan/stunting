f64daded3be6eee5ffab6a9d43a885cedd5a8108
ajil23 <azrilpraya@gmail.com>
2023-09-22 06:58:41 +0800

    user navbar Done

--- resources/views/user/component/navbar.blade.php
+++ resources/views/user/component/navbar.blade.php
@@ -1,25 +1,23 @@
 <section class="tombol fixed w-3/4 bottom-4 left-1/2 -translate-x-1/2 ">
     <div class="bg-white rounded-lg text-[30px] shadow-[0px_-3px_20px__rgba(0,0,0,0.5)]  py-1 grid grid-cols-4 gap-1">
-        <a href="">
-            <div class="text-center text-green-600">
+        <a href="{{ url('/user/home') }}">
+            <div class="text-center {{'user/home' == request()->path() ? 'text-green-600' : 'text-slate-600'}}">
                 <div><i class="fa-solid fa-house-user"></i></div>
-                
             </div>
         </a>
-        <a href="">
-            <div class="text-center text-slate-600">
+        <a href="{{ url('/user/bayi') }}">
+            <div class="text-center {{'user/bayi' == request()->path() ? 'text-green-600' : 'text-slate-600'}}">
                 <div><i class="fa-solid fa-baby"></i></div>
-                
             </div>
         </a>
-        <a href="">
-            <div class="text-center text-slate-600">
+        <a href="{{ url('/user/kalender') }}">
+            <div class="text-center {{'user/kalender' == request()->path() ? 'text-green-600' : 'text-slate-600'}}">
                 <div><i class="fa-solid fa-calendar"></i></div>
                 
             </div>
         </a>
-        <a href="">
-            <div class="text-center text-slate-600">
+        <a href="{{ url('/user/akun') }}">
+            <div class="text-center {{'user/akun' == request()->path() ? 'text-green-600' : 'text-slate-600'}}">
                 <div><i class="fa-solid fa-user"></i></div>
                 
             </div>
