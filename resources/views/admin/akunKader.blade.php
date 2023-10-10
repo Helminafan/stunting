91fca2c180c4b34232a8446a2e324e96918632c3
ajil23 <azrilpraya@gmail.com>
2023-10-10 14:12:53 +0800

    penyesuaian

--- resources/views/admin/akunKader.blade.php
+++ resources/views/admin/akunKader.blade.php
@@ -1,15 +1,13 @@
-@extends('user.master')
-
-@section('user')
-
+@extends('admin.master')
+@section('master')
 <section>
     <div class="container">
         <div class="navbar mb-5">
             <div class="bg-[#D981B4] items-center px-4 py-3 text-white flex">
-                <a href="{{ url('/user/akun') }}">
+                <a href="{{ url('/kader/akun') }}">
                     <i class="fa-solid fa-left-long mx-3"></i>
                 </a>
-                <a href="{{ url('/user/akun') }}">
+                <a href="{{ url('/kader/akun') }}">
                     <h1 class="font-semibold">Kembali</h1>
                 </a>
             </div>
@@ -55,13 +53,13 @@
     </div>
     <div class="mt-4 py-5 rounded-xl mx-8">
         <div class=" mx-3">
-            <a href="{{ url('/user/akunsaya/editprofil') }}">
+            <a href="">
                 <button type="button"
                 class=" w-full text-white bg-[#865BF4] hover:bg-[#865BF4] focus:outline-none focus:ring-4 focus:ring-[#865BF4] font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-[#865BF4] dark:hover:bg-[#865BF4]dark:focus:ring-[#865BF4]">Ganti Kontak</button>
             </a>
         </div>
         <div class=" mx-3">
-            <a href="{{ url('/user/akunsaya/gantipass') }}">
+            <a href="">
                 <button type="button"
                 class=" w-full text-white bg-[#D981B4] hover:bg-[#D981B4] focus:outline-none focus:ring-4 focus:ring-[#D981B4] font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-[#D981B4] dark:hover:bg-[#D981B4]dark:focus:ring-[#D981B4]">Ganti Password</button>
             </a>
