8bd6f2233c2c1ab206f099aa53186503f43d07a6
ajil23 <azrilpraya@gmail.com>
2023-10-19 12:18:54 +0800

    kesehatan bayi

---
+++ app/Http/Controllers/KesehatanBayiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Bayi;
+use App\Models\KesehatanBayi;
+
+class KesehatanBayiController extends Controller
+{
+    // function for "tambah data kesehatan bayi"
+    public function tambahDataKesehatanBayi(Request $request){
+        $dataKesehatanBayi = new KesehatanBayi();
+        $dataKesehatanBayi->tinggiBadanBayi = $request->tb;
+        $dataKesehatanBayi->beratBadanBayi = $request->bb;
+        $dataKesehatanBayi->lingkarKepala = $request->lingkep;
+        $dataKesehatanBayi->keterangan = $request->ket;
+        $dataKesehatanBayi->bayi_id = $request->bayi_id;
+        $dataKesehatanBayi->save();
+        return redirect()->route('dataBayi.view');
+    }
+}
