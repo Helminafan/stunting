22cc8c0351df6db458b03680de97554a5fe4f435
Helminafan <nafan2332@gmail.com>
2023-11-21 12:37:34 +0800

    update include

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -18,7 +18,7 @@
 <body class=" bg-[#F2F2F2]">
 
     @yield('user')
-    @extends('user.component.navbar')
+    @include('user.component.navbar')
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
